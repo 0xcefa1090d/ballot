1c38901d02f8d1e14e6cbe0812d94febf21abc7f
0xcefa1090d <126766757+0xcefa1090d@users.noreply.github.com>
2023-04-12 19:47:36 -0400

    Move utility functions to conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,4 +1,75 @@
+import functools
+
 import pytest
+import rlp
+
+
+@functools.singledispatch
+def unhexlify(obj):
+    raise NotImplementedError()
+
+
+@unhexlify.register(str)
+def _(hexstr):
+    match hexstr:
+        case "0x":
+            return b""
+        case "0x0":
+            return b"\x00"
+        case _ if len(hexstr := hexstr[2:]) % 2 != 0:
+            hexstr = "0" + hexstr
+
+    return bytes.fromhex(hexstr)
+
+
+@unhexlify.register(list)
+def _(obj):
+    return list(map(unhexlify, obj))
+
+
+@unhexlify.register(tuple)
+def _(obj):
+    return tuple(map(unhexlify, obj))
+
+
+def encode_block(block):
+    header = [
+        block["parentHash"],
+        block["sha3Uncles"],
+        block["miner"],
+        block["stateRoot"],
+        block["transactionsRoot"],
+        block["receiptsRoot"],
+        block["logsBloom"],
+        block["difficulty"],
+        block["number"],
+        block["gasLimit"],
+        block["gasUsed"],
+        block["timestamp"],
+        block["extraData"],
+        block["mixHash"],
+        block["nonce"],
+    ]
+
+    tmp = []
+    if withdrawals_root := block.get("withdrawlsRoot"):
+        tmp.insert(0, withdrawals_root or "0x")
+
+    if (base_fee_per_gas := block.get("baseFeePerGas")) or withdrawals_root:
+        tmp.insert(0, base_fee_per_gas or "0x")
+
+    return rlp.encode(unhexlify(header + tmp))
+
+
+def encode_receipt(receipt):
+    fields = [
+        receipt.get("root") or receipt["status"],
+        receipt["cumulativeGasUsed"],
+        receipt["logsBloom"],
+        [(log["address"], log["topics"], log["data"]) for log in receipt["logs"]],
+    ]
+
+    return unhexlify(receipt.get("type", "0x")) + rlp.encode(unhexlify(fields))
 
 
 @pytest.fixture(scope="session")
