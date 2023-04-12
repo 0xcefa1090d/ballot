620ac2dc5edadd8623c3f8e2bfa00cae6576dc7f
0xcefa1090d <126766757+0xcefa1090d@users.noreply.github.com>
2023-04-12 17:33:28 -0400

    Add encode_block function

---
+++ tests/new_vote_bounty/test_claim_bounty.py
@@ -0,0 +1,42 @@
+import rlp
+
+
+def unhexlify(hexstr):
+    match hexstr:
+        case "0x":
+            return b""
+        case "0x0":
+            return b"\x00"
+        case _ if len(hexstr := hexstr[2:]) % 2 != 0:
+            hexstr = "0" + hexstr
+
+    return bytes.fromhex(hexstr)
+
+
+def encode_block(block):
+    header = [
+        block["parentHash"],
+        block["sha3Uncles"],
+        block["miner"],
+        block["stateRoot"],
+        block["transactionsRoot"],
+        block["receiptsRoot"],
+        block["logsBloom"],
+        block["difficulty"],
+        block["number"],
+        block["gasLimit"],
+        block["gasUsed"],
+        block["timestamp"],
+        block["extraData"],
+        block["mixHash"],
+        block["nonce"],
+    ]
+
+    tmp = []
+    if withdrawals_root := block.get("withdrawlsRoot"):
+        tmp.insert(0, withdrawals_root or "0x")
+
+    if (base_fee_per_gas := block.get("baseFeePerGas")) or withdrawals_root:
+        tmp.insert(0, base_fee_per_gas or "0x")
+
+    return rlp.encode(list(map(unhexlify, header + tmp)))
