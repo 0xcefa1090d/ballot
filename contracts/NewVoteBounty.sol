a8633dab45fff4282c05b390056dc1117701cccf
0xcefa1090d <126766757+0xcefa1090d@users.noreply.github.com>
2023-04-12 05:11:54 -0400

    Add claimBounty method

--- contracts/NewVoteBounty.sol
+++ contracts/NewVoteBounty.sol
@@ -2,9 +2,14 @@
 pragma solidity 0.8.18;
 
 import {ERC20} from "transmissions11/solmate/tokens/ERC20.sol";
+import {IVoting} from "./interfaces/IVoting.sol";
+import {ReceiptProofVerifier} from "./libs/ReceiptProofVerifier.sol";
+import {RLPReader} from "hamdiallam/Solidity-RLP/RLPReader.sol";
 import {SafeTransferLib} from "transmissions11/solmate/utils/SafeTransferLib.sol";
 
 contract NewVoteBounty {
+    using RLPReader for bytes;
+    using RLPReader for RLPReader.RLPItem;
     using SafeTransferLib for ERC20;
 
     uint256 public constant OPEN_BOUNTY_COST = 1e16;
@@ -44,6 +49,65 @@ contract NewVoteBounty {
 
     constructor(address voting) {
         VOTING = voting;
+    }
+
+    function claimBounty(
+        address creator,
+        address rewardToken,
+        bytes32 digest,
+        uint256 receiptIndex,
+        bytes memory headerRlpBytes,
+        bytes memory proofRlpBytes
+    ) external {
+        bytes32 identifier = keccak256(
+            abi.encode(creator, rewardToken, digest)
+        );
+        Bounty memory bounty = getBounty[identifier];
+        require(bounty.amount != 0);
+
+        ReceiptProofVerifier.BlockHeader memory header = ReceiptProofVerifier
+            .verifyBlockHeader(headerRlpBytes);
+        require(bounty.timestamp < header.timestamp);
+
+        ReceiptProofVerifier.Receipt memory receipt = ReceiptProofVerifier
+            .extractReceiptFromProof(
+                receiptIndex,
+                header.receiptsRootHash,
+                proofRlpBytes.toRlpItem().toList()
+            );
+        require(receipt.status);
+
+        for (uint256 i = 0; i < receipt.logs.length; i++) {
+            if (receipt.logs[i].logger != VOTING) continue;
+            if (receipt.logs[i].topics[0] != START_VOTE_SIG) continue;
+
+            (string memory metadata, , , , ) = abi.decode(
+                receipt.logs[i].data,
+                (string, uint256, uint256, uint256, uint256)
+            );
+
+            uint256 voteId = uint256(receipt.logs[i].topics[1]);
+            (, , , , , , , , , bytes memory script) = IVoting(VOTING).getVote(
+                voteId
+            );
+
+            if (
+                keccak256(
+                    bytes.concat(keccak256(bytes(metadata)), keccak256(script))
+                ) != digest
+            ) continue;
+
+            delete getBounty[identifier];
+            getRefundAmount[creator] += OPEN_BOUNTY_COST;
+
+            address claimant = address(
+                uint160(uint256(receipt.logs[i].topics[2]))
+            );
+            ERC20(rewardToken).safeTransfer(claimant, bounty.amount);
+            emit ClaimBounty(identifier, claimant, voteId);
+            return;
+        }
+        revert();
     }
 
     function openBounty(
