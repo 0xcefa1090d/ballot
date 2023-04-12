2738018fab1c24706466bfa7ea0712837cd7d502
0xcefa1090d <126766757+0xcefa1090d@users.noreply.github.com>
2023-04-12 05:26:05 -0400

    Add token_mock and voting_mock test fixtures

--- tests/conftest.py
+++ tests/conftest.py
@@ -14,3 +14,13 @@ def bob(accounts):
 @pytest.fixture(scope="session")
 def charlie(accounts):
     yield accounts[2]
+
+
+@pytest.fixture(scope="module")
+def token_mock(alice, project):
+    yield project.TokenMock.deploy(10**24, sender=alice)
+
+
+@pytest.fixture(scope="module")
+def voting_mock(alice, project):
+    yield project.VotingMock.deploy(sender=alice)
