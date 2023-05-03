48c251e5410b090bc5ae69f67116ea016796ad40
0xcefa1090d <126766757+0xcefa1090d@users.noreply.github.com>
2023-05-02 23:31:47 -0400

    Add global flags

--- internal/cli/root.go
+++ internal/cli/root.go
@@ -6,12 +6,20 @@ import (
 	"github.com/spf13/cobra"
 )
 
+var (
+	node   string
+	signer string
+)
+
 var rootCmd = &cobra.Command{
 	Use:   "bounty",
 	Short: "the bounty command-line interface",
 }
 
 func init() {
+	rootCmd.PersistentFlags().StringVar(&node, "node", "https://eth.llamarpc.com", "set the `URL` of the execution layer node")
+	rootCmd.PersistentFlags().StringVar(&signer, "signer", "http://localhost:8550", "set the `URL` of the clef external signer")
+
 	rootCmd.AddCommand(openCmd)
 }
 
