302ebfb445fa6f209c9a6a0edbfdcd50ac498efe
0xcefa1090d <126766757+0xcefa1090d@users.noreply.github.com>
2023-05-04 01:16:11 -0400

    Move util functions to utils.go

--- internal/cli/open.go
+++ internal/cli/open.go
@@ -1,19 +1,13 @@
 package cli
 
 import (
-	"bufio"
-	"bytes"
-	"errors"
 	"fmt"
 	"log"
 	"math/big"
 	"os"
-	"strconv"
-	"strings"
 
 	"github.com/0xcefa1090d/bounty/internal/bindings"
 	"github.com/0xcefa1090d/bounty/internal/bindings/mocks"
-	"github.com/ethereum/go-ethereum/accounts"
 	"github.com/ethereum/go-ethereum/accounts/abi"
 	"github.com/ethereum/go-ethereum/accounts/abi/bind"
 	"github.com/ethereum/go-ethereum/accounts/external"
@@ -119,59 +113,5 @@ var openCmd = &cobra.Command{
 func init() {
 	if err := agentABI.UnmarshalJSON([]byte(rawAgentABI)); err != nil {
 		log.Fatal(err)
-	}
-}
-
-func prepareEVMScript(agent common.Address, script []action) []byte {
-	evmScript := []byte{00, 00, 00, 01}
-
-	for _, op := range script {
-		calldata, err := agentABI.Pack("execute", common.HexToAddress(op.Target), big.NewInt(0), common.FromHex(op.Calldata))
-		if err != nil {
-			log.Fatal(err)
-		}
-		length := common.LeftPadBytes(big.NewInt(int64(len(calldata))).Bytes(), 8)
-		evmScript = bytes.Join([][]byte{evmScript, agent.Bytes(), length, calldata}, []byte{})
-	}
-	return evmScript
-}
-
-func selectAccount(extSigner *external.ExternalSigner) (accounts.Account, error) {
-	if accountAddr != "" {
-		account := accounts.Account{Address: common.HexToAddress(accountAddr), URL: accounts.URL{}}
-		if extSigner.Contains(account) {
-			return account, nil
-		}
-		return accounts.Account{}, fmt.Errorf("the provided account is not available: %s", accountAddr)
-	}
-
-	available := extSigner.Accounts()
-	switch len(available) {
-	case 0:
-		return accounts.Account{}, errors.New("the external signer does not have any available accounts")
-	case 1:
-		return available[0], nil
-	default:
-		for idx, acct := range available {
-			fmt.Printf("[%d]: %s\n", idx, acct.Address)
-		}
-
-		fmt.Print("Select an available account: ")
-
-		selection, err := bufio.NewReader(os.Stdin).ReadString('\n')
-		if err != nil {
-			return accounts.Account{}, err
-		}
-
-		idx, err := strconv.Atoi(strings.TrimSpace(selection))
-		if err != nil {
-			return accounts.Account{}, err
-		}
-
-		if idx < 0 || len(available) < idx {
-			return accounts.Account{}, errors.New("invalid selection")
-		}
-
-		return available[idx], nil
 	}
 }
